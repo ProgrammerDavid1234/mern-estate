53a9283aa727c63cad12371e2ba4603da5d1ed1f
ProgrammerDavid1234 <olonadenifemi@gmail.com>
2024-01-12 13:42:47 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,10 +1,27 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO;
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+  }
+};
+
+connectToDatabase();
 
 const app = express();
 
-app.listen(3000,() =>{
-    console.log('Server is running on port 3000 !!')
-} 
-
-
-);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
