51da459c1cb9a5b1af3d0b8cf8dc0206cbf920f6
ProgrammerDavid1234 <olonadenifemi@gmail.com>
2024-01-17 21:46:58 +0100

    complete sign up page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
             <Link to='/'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>OLONADE'S</span>
+            <span className='text-slate-500'>ECHS</span>
             <span className='text-slate-700'>ESTATE</span>
             </h1>   
             </Link>     
